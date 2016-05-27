e46267a2bfc4a782cbf3b2ce4261642609049f6e
nvisanji <nvisanj@nadeem-v.local>
2016-05-27 11:55:38 -0700

    Initial Commit

---
+++ Week 2 HomeworkTests/Week_2_HomeworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_2_HomeworkTests.swift
+//  Week 2 HomeworkTests
+//
+//  Created by Nadeem Visanji on 5/27/16.
+//  Copyright © 2016 Nadeem Visanji. All rights reserved.
+//
+
+import XCTest
+@testable import Week_2_Homework
+
+class Week_2_HomeworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
